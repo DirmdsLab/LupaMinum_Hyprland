616a5facdc8153a13d0b817f6ebf27d634cedc2e
DirmdsLab <141147147+DirmdsLab@users.noreply.github.com>
2026-03-30 14:21:27 +0700

    setup rename

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e
 # Paths & State
 # =========================
 ROOT_DIR="$(pwd)"
-TEMP_DIR="$ROOT_DIR/Temp"
+TEMP_DIR="$ROOT_DIR/temp"
 STATE_FILE="$TEMP_DIR/UwU.temp"
 LOG_FILE="$TEMP_DIR/setup.log"
 
@@ -57,70 +57,70 @@ always_run() {
 
     # Tmux
     run rm -rf "$HOME/.tmux.conf"
-    run ln -sf "$ROOT_DIR/User/.tmux.conf" "$HOME/.tmux.conf"
+    run ln -sf "$ROOT_DIR/user/.tmux.conf" "$HOME/.tmux.conf"
 
     # Nano
     run rm -rf "$HOME/.nanorc"
-    run ln -sf "$ROOT_DIR/User/.nanorc" "$HOME/.nanorc"
+    run ln -sf "$ROOT_DIR/user/.nanorc" "$HOME/.nanorc"
 
     # btop
     run rm -rf "$HOME/.config/btop"
-    run ln -sf "$ROOT_DIR/User/.config/btop" "$HOME/.config/btop"
+    run ln -sf "$ROOT_DIR/user/.config/btop" "$HOME/.config/btop"
 
     # cava
     run rm -rf "$HOME/.config/cava"
-    run ln -sf "$ROOT_DIR/User/.config/cava" "$HOME/.config/cava"
+    run ln -sf "$ROOT_DIR/user/.config/cava" "$HOME/.config/cava"
 
     # Fish
     rm -rf "$HOME/.config/fish"
     mkdir -p "$HOME/.config/fish"
-    ln -sf "$ROOT_DIR/User/.config/fish/config.fish" "$HOME/.config/fish/config.fish"
+    ln -sf "$ROOT_DIR/user/.config/fish/config.fish" "$HOME/.config/fish/config.fish"
 
     # foot
     run rm -rf "$HOME/.config/foot"
-    run ln -sf "$ROOT_DIR/User/.config/foot" "$HOME/.config/foot"
+    run ln -sf "$ROOT_DIR/user/.config/foot" "$HOME/.config/foot"
 
     # hypr
     run rm -rf "$HOME/.config/hypr"
-    run ln -sf "$ROOT_DIR/User/.config/hypr" "$HOME/.config/hypr"
+    run ln -sf "$ROOT_DIR/user/.config/hypr" "$HOME/.config/hypr"
 
     # Kitty
     run rm -rf "$HOME/.config/kitty"
-    run ln -sf "$ROOT_DIR/User/.config/kitty" "$HOME/.config/kitty"
+    run ln -sf "$ROOT_DIR/user/.config/kitty" "$HOME/.config/kitty"
 
     # mako
     run rm -rf "$HOME/.config/mako"
-    run ln -sf "$ROOT_DIR/User/.config/mako" "$HOME/.config/mako"
+    run ln -sf "$ROOT_DIR/user/.config/mako" "$HOME/.config/mako"
 
     # mpv
-    run rm -f "$ROOT_DIR/User/.config/mpv/playlists"
-    run ln -s "$HOME/playlist" "$ROOT_DIR/User/.config/mpv/playlists"
+    run rm -f "$ROOT_DIR/user/.config/mpv/playlists"
+    run ln -s "$HOME/Playlists" "$ROOT_DIR/user/.config/mpv/playlists"
     run rm -rf "$HOME/.config/mpv"
-    run ln -sf "$ROOT_DIR/User/.config/mpv" "$HOME/.config/mpv"
+    run ln -sf "$ROOT_DIR/user/.config/mpv" "$HOME/.config/mpv"
 
     # neofetch
     run rm -rf "$HOME/.config/neofetch"
-    run ln -sf "$ROOT_DIR/User/.config/neofetch" "$HOME/.config/neofetch"
+    run ln -sf "$ROOT_DIR/user/.config/neofetch" "$HOME/.config/neofetch"
 
     # quickshell
     run rm -rf "$HOME/.config/quickshell"
-    run ln -sf "$ROOT_DIR/User/.config/quickshell" "$HOME/.config/quickshell"
+    run ln -sf "$ROOT_DIR/user/.config/quickshell" "$HOME/.config/quickshell"
 
     # wofi
     run rm -rf "$HOME/.config/wofi"
-    run ln -sf "$ROOT_DIR/User/.config/wofi" "$HOME/.config/wofi"
+    run ln -sf "$ROOT_DIR/user/.config/wofi" "$HOME/.config/wofi"
 
     # mimeapps.list
     run rm -rf "$HOME/.config/mimeapps.list"
-    run ln -sf "$ROOT_DIR/User/.config/mimeapps.list" "$HOME/.config/mimeapps.list"
+    run ln -sf "$ROOT_DIR/user/.config/mimeapps.list" "$HOME/.config/mimeapps.list"
 
     # starship.toml
     run rm -rf "$HOME/.config/starship.toml"
-    run ln -sf "$ROOT_DIR/User/.config/starship.toml" "$HOME/.config/starship.toml"
+    run ln -sf "$ROOT_DIR/user/.config/starship.toml" "$HOME/.config/starship.toml"
 
     # Script
     run rm -rf "$HOME/File/Script"
-    run ln -sf "$ROOT_DIR/User/File/Script" "$HOME/File/Script"
+    run ln -sf "$ROOT_DIR/user/File/Script" "$HOME/File/Script"
 
     log "=== ALWAYS RUN TASKS END ==="
 }
@@ -140,7 +140,7 @@ first_setup_only() {
     run mkdir -p "$HOME/Pictures"
     run mkdir -p "$HOME/Pictures/Screenshot"
     
-    run mkdir -p "$HOME/playlist"
+    run mkdir -p "$HOME/Playlists"
 
     run mkdir -p "$HOME/Videos"
     run mkdir -p "$HOME/Videos/Wallpaper"
@@ -156,7 +156,7 @@ first_setup_only() {
     run mkdir -p "$HOME/File/Software/Storage/temphdd"
 
     # .local
-    run cp -r "$ROOT_DIR/User/.local" "$HOME/"
+    run cp -r "$ROOT_DIR/user/.local" "$HOME/"
 
     # Tmux
     run rm -rf "$HOME/.config/tmux"
@@ -166,34 +166,34 @@ first_setup_only() {
     run mv "$ROOT_DIR/external/tmux/tmux-main" "$HOME/.config/tmux/plugins/catppuccin/tmux"
 
     # Mpv shaders
-    run rm -rf OtherRepo/GLSL_Mac_Linux_High-end
-    run mkdir -p OtherRepo/GLSL_Mac_Linux_High-end
-    run unzip "$ROOT_DIR/OtherRepo/GLSL_Mac_Linux_High-end.zip" -d "OtherRepo/GLSL_Mac_Linux_High-end"
-    run rm -rf "$ROOT_DIR/User/.config/mpv/shaders"
-    run mv "$ROOT_DIR/OtherRepo/GLSL_Mac_Linux_High-end/shaders" "$ROOT_DIR/User/.config/mpv/"
+    run rm -rf external/mpv/GLSL_Mac_Linux_High-end
+    run mkdir -p external/mpv/GLSL_Mac_Linux_High-end
+    run unzip "$ROOT_DIR/external/mpv/GLSL_Mac_Linux_High-end.zip" -d "external/mpv/GLSL_Mac_Linux_High-end"
+    run rm -rf "$ROOT_DIR/user/.config/mpv/shaders"
+    run mv "$ROOT_DIR/external/mpv/GLSL_Mac_Linux_High-end/shaders" "$ROOT_DIR/user/.config/mpv/"
 
     # themes
 
     run mkdir -p "$HOME/.themes"
 
-    run rm -rf "$ROOT_DIR/OtherRepo/Theme/gtk/Graphite-Dark-nord"
-    run mkdir -p "$ROOT_DIR/OtherRepo/Theme/gtk/Graphite-Dark-nord"
-    run tar -xf "$ROOT_DIR/OtherRepo/Theme/gtk/Graphite-Dark-nord.tar.xz" -C "$ROOT_DIR/OtherRepo/Theme/gtk/Graphite-Dark-nord"
-    run cp -r "$ROOT_DIR/OtherRepo/Theme/gtk/Graphite-Dark-nord/Graphite-Dark-nord" "$HOME/.themes"
+    run rm -rf "$ROOT_DIR/external/themes/gtk/Graphite-Dark-nord"
+    run mkdir -p "$ROOT_DIR/external/themes/gtk/Graphite-Dark-nord"
+    run tar -xf "$ROOT_DIR/external/themes/gtk/Graphite-Dark-nord.tar.xz" -C "$ROOT_DIR/external/themes/gtk/Graphite-Dark-nord"
+    run cp -r "$ROOT_DIR/external/themes/gtk/Graphite-Dark-nord/Graphite-Dark-nord" "$HOME/.themes"
 
     # Cursor
     run mkdir -p "$HOME/.icons"
 
-    run rm -rf "$ROOT_DIR/OtherRepo/Theme/Cursor/Bibata-Modern-Ice"
-    run mkdir -p "$ROOT_DIR/OtherRepo/Theme/Cursor/Bibata-Modern-Ice"
-    run tar -xf "$ROOT_DIR/OtherRepo/Theme/Cursor/Bibata-Modern-Ice.tar.xz" -C "$ROOT_DIR/OtherRepo/Theme/Cursor/Bibata-Modern-Ice"
-    run cp -r "$ROOT_DIR/OtherRepo/Theme/Cursor/Bibata-Modern-Ice/Bibata-Modern-Ice" "$HOME/.icons"
+    run rm -rf "$ROOT_DIR/external/themes/cursor/Bibata-Modern-Ice"
+    run mkdir -p "$ROOT_DIR/external/themes/cursor/Bibata-Modern-Ice"
+    run tar -xf "$ROOT_DIR/external/themes/cursor/Bibata-Modern-Ice.tar.xz" -C "$ROOT_DIR/external/themes/cursor/Bibata-Modern-Ice"
+    run cp -r "$ROOT_DIR/external/themes/cursor/Bibata-Modern-Ice/Bibata-Modern-Ice" "$HOME/.icons"
 
     # Icon
-    run rm -rf "$ROOT_DIR/OtherRepo/Theme/Icon/01-Colloid"
-    run mkdir -p "$ROOT_DIR/OtherRepo/Theme/Icon/01-Colloid"
-    run tar -xf "$ROOT_DIR/OtherRepo/Theme/Icon/01-Colloid.tar.xz" -C "$ROOT_DIR/OtherRepo/Theme/Icon/01-Colloid"
-    run cp -r $ROOT_DIR/OtherRepo/Theme/Icon/01-Colloid/* "$HOME/.icons"
+    run rm -rf "$ROOT_DIR/external/themes/icon/01-Colloid"
+    run mkdir -p "$ROOT_DIR/external/themes/icon/01-Colloid"
+    run tar -xf "$ROOT_DIR/external/themes/icon/01-Colloid.tar.xz" -C "$ROOT_DIR/external/themes/icon/01-Colloid"
+    run cp -r $ROOT_DIR/external/themes/icon/01-Colloid/* "$HOME/.icons"
 
     # To apply themes
     run echo "Run 'nwg-look' to apply themes"
